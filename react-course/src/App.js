ab995eb2b21397924ee379444b145460893d3584
Leon Gabriel <g73346920@gmail.com>
2024-09-11 23:30:42 +0300

    Modified

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,10 +1,14 @@
-
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount] = useState(0)
   return (
     <div className="App">
-   
+      <div>current value {count}</div>
+   <button  value={count} onClick={() => setCount(count + 1)}>Increase</button>
+   <button value={count}  onClick={() => setCount(count - 1)}>Decrease</button>
+   <button value={count}  onClick={() => setCount(0)}>Set to Zero</button>
     </div>
   );
 }
