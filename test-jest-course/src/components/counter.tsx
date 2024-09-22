d3faac4aeadbae2ae1bd185c7656f69495dc67a0
Leon Gabriel <g73346920@gmail.com>
2024-09-22 17:29:16 +0300

    Chored:3 buttons included

--- test-jest-course/src/components/counter.tsx
+++ test-jest-course/src/components/counter.tsx
@@ -1,8 +1,16 @@
 import React, {useState} from 'react'
 
-const Counter = () => {
+const Counter = ({initialCount}) => {
+  const [count,setCount] = useState(initialCount)
   return (
-    <div>counter</div>
+    <div>
+      <h1>Count:{count}</h1>
+
+      <button>Increanment</button>
+      <button>Decreament</button>
+      <button>Reset</button>
+      <button>SwitchSign</button>
+    </div>
   )
 }
 
