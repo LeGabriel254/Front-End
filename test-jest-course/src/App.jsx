2b0d6598391b95358c42d6ec6023c12f6556aa2e
Leon Gabriel <g73346920@gmail.com>
2024-09-22 17:25:16 +0300

    Initial commit

---
+++ test-jest-course/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './components/counter'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ test-jest-course/src/components/counter.tsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+
+const Counter = () => {
+  return (
+    <div>counter</div>
+  )
+}
+
+export default Counter
