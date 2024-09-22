34c9818240bbeee054d90de0e1a9bdbf9de0fd2e
Leon Gabriel <g73346920@gmail.com>
2024-09-22 17:42:28 +0300

    Chored:Button function rendered

--- test-jest-course/src/components/counter.tsx
+++ test-jest-course/src/components/counter.tsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react'
 
-const Counter = ({initialCount}) => {
-  const [count,setCount] = useState(initialCount)
+const Counter = () => {
+  const [count,setCount] = useState(0)
+  
   return (
     <div>
       <h1>Count:{count}</h1>
 
-      <button>Increanment</button>
-      <button>Decreament</button>
-      <button>Reset</button>
-      <button>SwitchSign</button>
+      <button value={count} onClick={() => setCount(count + 1)}>Increanment</button>
+      <button value={count} onClick={() => setCount(count - 1)}>Decreament</button>
+      <button value={count} onClick={() => setCount(0)}>Reset</button>
+      <button value={count} onClick={() => setCount((prev) => prev * -1)}>SwitchSign</button>
     </div>
   )
 }
