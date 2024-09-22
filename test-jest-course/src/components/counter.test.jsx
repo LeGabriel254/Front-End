0d9d48d2bd475bca4438326c5e375e9373808c6e
Leon Gabriel <g73346920@gmail.com>
2024-09-22 18:32:08 +0300

    Test to check if the Decrement button works

--- test-jest-course/src/components/counter.test.jsx
+++ test-jest-course/src/components/counter.test.jsx
@@ -3,13 +3,13 @@ import Counter from './counter';
 
 // Test to check if the counter renders correctly
 test('render counter components', () => {
-render(<Counter />)
- // verify component initial render
- expect(screen.getByText(/Count: 0/i)).toBeInTheDocument();
- expect(screen.getByText('Increment')).toBeInTheDocument();
- expect(screen.getByText('Decrement')).toBeInTheDocument();
- expect(screen.getByText('Reset')).toBeInTheDocument();
- expect(screen.getByText('SwitchSign')).toBeInTheDocument();
+  render(<Counter />)
+  // verify component initial render
+  expect(screen.getByText(/Count: 0/i)).toBeInTheDocument();
+  expect(screen.getByText('Increment')).toBeInTheDocument();
+  expect(screen.getByText('Decrement')).toBeInTheDocument();
+  expect(screen.getByText('Reset')).toBeInTheDocument();
+  expect(screen.getByText('SwitchSign')).toBeInTheDocument();
 });
 
 //Test to check if the increment button works
