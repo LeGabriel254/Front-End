d31a0aebad161d37043d16ad81d0c215eae30ea2
Leon Gabriel <g73346920@gmail.com>
2024-09-23 23:51:46 +0300

    Modified

--- test-jest-course/src/components/counter.test.jsx
+++ test-jest-course/src/components/counter.test.jsx
@@ -2,22 +2,22 @@ import { render, screen, fireEvent, getAllByText } from '@testing-library/react'
 import Counter from './counter';
 
 // Test to check if the counter renders correctly
-test('render Counter components', () => {
-  render(<Counter />)
-  // verify component initial render
-  expect(screen.getByText(/Count: 0/i)).toBeInTheDocument();
-  expect(screen.getByText('Increment')).toBeInTheDocument();
-  expect(screen.getByText('Decrement')).toBeInTheDocument();
-  expect(screen.getByText('Reset')).toBeInTheDocument();
-  expect(screen.getByText('SwitchSign')).toBeInTheDocument();
-});
+describe('Counter', () => {
+
+  fetchMock.get('*', some_response);
+
+  it('renders something', () => {
+      let wrapper = mount(<Counter />);
+      expect(wrapper.find()).to.have.something();
+  });
+})
 
 //Test to check if the increment button works
 test('Increment counter', () => {
   render(<Counter />)
   //Initialise click Event
   fireEvent.click(screen.getAllByText('increment'))
-  expect(screen.getByText(/Count: 1/i)).toBeInTheDocument()
+  expect(screen.getAllByText(/Count: 1/i)).toBeInTheDocument()
 });
 //Test to check if the Decrement button works
 
