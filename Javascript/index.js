8b9807eebefbd865ddfdcb7d6f98c663234f7599
Leon Gabriel <g73346920@gmail.com>
2024-10-14 14:01:56 +0300

    Chored:Callback function rendered

--- Javascript/index.js
+++ Javascript/index.js
@@ -32,3 +32,38 @@ const truck =  Object.create(vehicle)
 truck.door = 2
 console.log(truck.door)
 console.log(truck.engine())
+
+
+
+//Walk the dog
+//clean the kitchen
+// take ut the ctrash
+
+function walkDog(callback){
+  setTimeout(() => {
+    console.log("You walk the dog")
+    callback()
+  },1500)
+};
+
+function cleanKitchen(callback){
+  setTimeout(() => {
+    console.log("You cleaned the kitchen")
+    callback()
+  },2500)
+};
+
+function collectTrash(callback){
+  setTimeout(() => {
+    console.log("You collected the trash")
+    callback()
+  },1500)
+};
+
+walkDog(() => {
+   cleanKitchen(() => {
+      collectTrash(() =>{
+         console.log("Completd chores")
+      })
+   })
+})
