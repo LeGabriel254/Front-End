5795f23f2ea6dca0f02167fb56e3f314d6f9bf0b
Leon Gabriel <g73346920@gmail.com>
2024-10-14 12:57:47 +0300

    Modiifed

--- Javascript/index.js
+++ Javascript/index.js
@@ -9,3 +9,26 @@ for(let fruit of fruits){
 console.log(fruit);
 
 }
+
+// oject are key values pairs in curlbraces
+let user = {
+  name:"Gabriel",
+  age:23,
+  Country:"Kenya"
+}
+
+console.log(user.age);
+
+//Inheritance 
+let vehicle = {
+  type:"BMW",
+  wheels:4,
+  engine: function(){
+    return"Vrooom"
+  }
+} 
+// console.log(vehicle.engine)
+const truck =  Object.create(vehicle)
+truck.door = 2
+console.log(truck.door)
+console.log(truck.engine())
