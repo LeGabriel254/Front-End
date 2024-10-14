714a7c527cac37aa4bf72997e3d23c4798d6f823
Leon Gabriel <g73346920@gmail.com>
2024-10-14 12:37:10 +0300

    Initial commit

---
+++ Javascript/index.js
@@ -0,0 +1,11 @@
+// arrays this are variable structure that carries more than one value
+
+let fruits = ["Mangoe", "Avocado", "Banana","Melon"]
+
+fruits.sort().reverse();
+
+for(let fruit of fruits){
+
+console.log(fruit);
+
+}
