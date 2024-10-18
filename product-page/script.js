d1f5f50182416920676fdc5a06a3da4b2af5b0f5
Leon Gabriel <g73346920@gmail.com>
2024-10-19 00:33:21 +0300

    I

--- product-page/script.js
+++ product-page/script.js
@@ -8,28 +8,42 @@ document.addEventListener('DOMContentLoaded', function(){
   }
  
   //DOM Elements
-  const button = document.getElementById('bookmark');
+  const button = document.getElementById('button');
   const support = document.getElementById('support');
   const got =  document.getElementById('got');
   const close =  document.getElementById('close')
   const secondary = document.getElementById('secondary')
   const project = document.getElementById('project')
-// When the user clicks the bookmark button, open the modal
+  const container = document.getElementById('container')
+
+
+  // When the user clicks the bookmark button, open the modal
 button.onclick = function() {
   support.style.display = "block";
+  
 }
+
+
 //When the user clicks the got, the support closes
 got.onclick = function(){
   support.style.display = "none"
 }
+
+
 // when the user clicks the secondary button the projects open
 secondary.onclick = function(){
 project.style.display ="block"
+
 }
+
+
 //when the user clicks close(x) the project close
 close.onclick = function (){
   project.style.display = "none"
 }
+
+
+
 // When the user clicks anywhere outside of the support, close it
 window.onclick = function(event) {
   if (event.target == support) {
