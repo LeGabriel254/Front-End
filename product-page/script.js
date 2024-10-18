2839c9e7f395a5727eefc3c46023b62bc74b1e8f
Leon Gabriel <g73346920@gmail.com>
2024-10-18 16:45:38 +0300

    chored:Function to ensure the Js code runs after the Html fullyLoaded.

--- product-page/script.js
+++ product-page/script.js
@@ -1,6 +1,28 @@
-function toggleMenu() {
-  const menu = document.getElementById("nav-links");
-  const icon = document.getElementById("hamburger-icon")
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+//JavaScript code runs after the html has fullyloaded
+document.addEventListener('DOMContentLoaded', function(){
+  
+  function toggleMenu() {
+    const menu = document.getElementById("nav-links");
+    const icon = document.getElementById("hamburger-icon")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  //DOM Elements
+  const button = document.getElementById('bookmark');
+  const support = document.getElementById('support');
+  const close = document.getElementsByClassName('close');
+  const got =  document.getElementById('got');
+
+
+// When the user clicks the bookmark button, open the modal
+button.onclick = function() {
+  support.style.display = "block";
 }
+
+//When the user clicks the got, the support closes
+got.onclick = function(){
+  support.style.display = "none"
+}
+
+})
