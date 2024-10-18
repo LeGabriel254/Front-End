3c990a0fc6a25bdbefcfdc38b2f3a2cea8fd7d8a
Leon Gabriel <g73346920@gmail.com>
2024-10-18 21:15:47 +0300

    chored:when the user clicks close(x) the project close.

--- product-page/script.js
+++ product-page/script.js
@@ -11,6 +11,9 @@ document.addEventListener('DOMContentLoaded', function(){
   const button = document.getElementById('bookmark');
   const support = document.getElementById('support');
   const got =  document.getElementById('got');
+  const close =  document.getElementById('close')
+  const secondary = document.getElementById('secondary')
+  const project = document.getElementById('project')
 // When the user clicks the bookmark button, open the modal
 button.onclick = function() {
   support.style.display = "block";
@@ -18,6 +21,14 @@ button.onclick = function() {
 //When the user clicks the got, the support closes
 got.onclick = function(){
   support.style.display = "none"
+}
+// when the user clicks the secondary button the projects open
+secondary.onclick = function(){
+project.style.display ="block"
+}
+//when the user clicks close(x) the project close
+close.onclick = function (){
+  project.style.display = "none"
 }
 // When the user clicks anywhere outside of the support, close it
 window.onclick = function(event) {
