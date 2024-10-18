fc3ab6a7e6f9bc097d1c37aa68cd1fe39e00abd7
Leon Gabriel <g73346920@gmail.com>
2024-10-18 14:55:46 +0300

    Initial commit

--- product-page/script.js
+++ product-page/script.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-  const menu = document.getElementById("menu-links");
+  const menu = document.getElementById("nav-links");
   const icon = document.getElementById("hamburger-icon")
   menu.classList.toggle("open");
   icon.classList.toggle("open");
