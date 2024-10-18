8387f389396204066eb670b7ec0d507b141623f6
Leon Gabriel <g73346920@gmail.com>
2024-10-18 16:48:01 +0300

    Chored when the bookmark button is clicked the support displays to block.

--- product-page/script.js
+++ product-page/script.js
@@ -7,19 +7,14 @@ document.addEventListener('DOMContentLoaded', function(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-
   //DOM Elements
   const button = document.getElementById('bookmark');
   const support = document.getElementById('support');
-  const close = document.getElementsByClassName('close');
   const got =  document.getElementById('got');
-
-
 // When the user clicks the bookmark button, open the modal
 button.onclick = function() {
   support.style.display = "block";
 }
-
 //When the user clicks the got, the support closes
 got.onclick = function(){
   support.style.display = "none"
