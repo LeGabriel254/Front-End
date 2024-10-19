c227c7e8bd931829d35485ae76e1cfc8ad265630
Leon Gabriel <g73346920@gmail.com>
2024-10-19 15:42:40 +0300

    chored: // When the user clicks the bookmark button, open the modal

--- product-page/script.js
+++ product-page/script.js
@@ -1,59 +1,65 @@
+
+
 //JavaScript code runs after the html has fullyloaded
-document.addEventListener('DOMContentLoaded', function(){
-  function toggleMenu() {
-    const menu = document.getElementById("nav-links");
-    const icon = document.getElementById("hamburger-icon")
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
- 
-  function btn(){
-      const button = document.getElementById('button');
-        const support = document.getElementById('support');
-        button.classList.onclick = support
-  }
+
+document.addEventListener('DOMContentLoaded', () => {
+  
   //DOM Elements
   const button = document.getElementById('button');
   const support = document.getElementById('support');
-  const got =  document.getElementById('got');
-  const close =  document.getElementById('close')
+  const got = document.getElementById('got');
+  const close = document.getElementById('close')
   const secondary = document.getElementById('secondary')
   const project = document.getElementById('project')
   const container = document.getElementById('container')
 
+  function toggleMenu() {
 
-  // When the user clicks the bookmark button, open the modal
-button.onclick = function() {
-  support.style.display = "block";
+    const menu = document.getElementById("nav-links");
+    const icon = document.getElementById("hamburger-icon")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+  }
+
+  function handleModalAction(modalClass, action) {
+
+    if (action === 'open') {
+      // document.body.classList.add( 'blur-backdrop')
+      document.body.classList.add('lock-scroll')
+      modalClass.open = true;
+    } else {
+      modalClass.open = false
+      document.body.classList.remove('lock-scroll')
+    }
+  }
   
-}
+  // When the user clicks the bookmark button, open the modal
+  button.onclick = () => {
+    handleModalAction(support, 'open')
+  }
 
+  //When the user clicks the got, the support closes
+  got.onclick = () => {
+    handleModalAction(support, 'close')
+  }
 
-//When the user clicks the got, the support closes
-got.onclick = function(){
-  support.style.display = "none"
-}
+  support.onmouseleave = function () {
+    handleModalAction(support, 'close')
+  }
 
+  // when the user clicks the secondary button the projects open
+  secondary.onclick = () => {
+    project.style.display = "block"
+  }
 
-// when the user clicks the secondary button the projects open
-secondary.onclick = function(){
-project.style.display ="block"
-document.body.classList.add('scrolling');
-}
+  //when the user clicks close(x) the project close
+  close.onclick = () => {
+    project.style.display = "none"
+  }
 
-
-//when the user clicks close(x) the project close
-close.onclick = function (){
-  project.style.display = "none"
-}
-
-
-
-// When the user clicks anywhere outside of the support, close it
-// window.onclick = function(event) {
-//   if (event.target == support) {
-//       support.style.display = "none";
-//   }
-// }
+  window.onmousedown = () => {
+    handleModalAction(support, 'close')
+  }
 
 })
