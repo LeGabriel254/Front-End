76ae98de657b7ba6bd41c679f5bf1cc7ebe7ff86
Leon Gabriel <g73346920@gmail.com>
2024-10-22 22:01:06 +0300

    Timeout to Reload the page to start fresh.

--- product-page/script.js
+++ product-page/script.js
@@ -1,7 +1,5 @@
 
-
 //JavaScript code runs after the html has fullyloaded
-
 document.addEventListener('DOMContentLoaded', () => {
   
   //DOM Elements
@@ -12,8 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const secondary = document.getElementById('secondary')
   const project = document.getElementById('project')
   const succefully= document.getElementById('continue')
-  const hold = document.getElementById('holder')
-  // const container = document.getElementById('container')
 
   function toggleMenu() {
 
@@ -22,53 +18,50 @@ document.addEventListener('DOMContentLoaded', () => {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 
-  }
+  };
 
   function handleModalAction(modalClass, action) {
 
     if (action === 'open') {
    
-      document.body.classList.add('lock-scroll')
+      document.body.classList.add('lock-scroll');
       modalClass.open = true;
     } else {
       modalClass.open = false
-      document.body.classList.remove('lock-scroll')
+      document.body.classList.remove('lock-scroll');
     }
     
-  }
+  };
   
   // When the user clicks the bookmark button, open the modal
   succefully.onclick = () => {
-    handleModalAction(support, 'open')
-  }
+    handleModalAction(support, 'open');
+  };
 
   button.onclick = () => {
-    handleModalAction(support, 'open')
-  }
+    handleModalAction(support, 'open');
+  };
   //When the user clicks the got, the support closes
   got.onclick = () => {
     handleModalAction(support, 'close')
     setTimeout( () => {
       location.reload(); // Reload the page to start fresh
-  }, 2000); // Waits for 2 seconds before reloading
-  }
+  }, 2000); 
+};
  
-  // support.onmouseleave = function () {
-  //   handleModalAction(support, 'close')
-  // }
 
   // when the user clicks the secondary button the projects open
   secondary.onclick = () => {
-    handleModalAction(project, 'open')
-  }
+    handleModalAction(project, 'open');
+  };
   
   //when the user clicks close(x) the project close
   close.onclick = () => {
-    handleModalAction( project, 'close')
-  }
+    handleModalAction( project, 'close');
+  };
 
   window.onmousedown = () => {
-    handleModalAction(support, 'close')
-  }
+    handleModalAction(support, 'close');
+  };
 
-})
+});
